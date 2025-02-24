56c4bfcd0d82defd5dee4270c71dd06c632e9fa3
prakhar-174 <prakhartiwari174@gmail.com>
2025-02-25 04:15:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Optional JavaScript for potential interactivity
+document.querySelector('.customization-button').addEventListener('click', function() {
+    alert('Customize header navigation clicked');
+});
