d6c0a7e7aa181570a30788a6d22416f61900f40e
prakhar-174 <prakhartiwari174@gmail.com>
2025-02-26 03:53:58 +0530

    Past Events Added

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 document.querySelector('.customization-button').addEventListener('click', function() {
     alert('Customize header navigation clicked');
 });
+
+// Set initial state for about-card
+gsap.set(".about-card", { y: 0 });
+
+// Floating in air effect for about-card
+gsap.to(".about-card", {
+    y: -20,
+    repeat: -1,
+    yoyo: true,
+    duration: 1.5,
+    ease: "power1.inOut",
+});
