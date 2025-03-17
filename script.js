1f80044b865524b898d289eeae76474391a566d9
prakhar-174 <prakhartiwari174@gmail.com>
2025-03-18 02:22:27 +0530

    Hamburger Menu Revised

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const hamburgerInput = document.querySelector(".hamburger input");
-const navLinks = document.querySelector(".nav-links");
+// const hamburgerInput = document.querySelector(".hamburger input");
+// const navLinks = document.querySelector(".nav-links");
 
-console.log(hamburgerInput);
+// console.log(hamburgerInput);
 
 
-hamburgerInput.addEventListener("change", () => {
-  if (hamburgerInput.checked) {    
-    navLinks.classList.add("active");
-  } else {    
-    navLinks.classList.remove("active");
-  }
-});
+// hamburgerInput.addEventListener("change", () => {
+//   if (hamburgerInput.checked) {    
+//     navLinks.classList.add("active");
+//   } else {    
+//     navLinks.classList.remove("active");
+//   }
+// });
 
-// Close menu when clicking nav links
-document.querySelectorAll(".nav-links a").forEach(link => {
-  link.addEventListener("click", () => {
-    hamburgerInput.checked = false;
-    navLinks.classList.remove("active");
-  });
-});
+
+// document.querySelectorAll(".nav-links a").forEach(link => {
+//   link.addEventListener("click", () => {
+//     hamburgerInput.checked = false;
+//     navLinks.classList.remove("active");
+//   });
+// });
 
 
 let originalContent = null;
